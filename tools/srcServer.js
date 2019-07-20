d2b924479437110cd30e344d0b93e747c81c0f8c
AhyoungRyu <ahyoungryu@apache.org>
2019-07-20 17:20:33 +0900

    Trial to setup WS proxy ver2

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -19,37 +19,37 @@ browserSync({
   ui: {
     port: 3001,
   },
-  server: {
-    baseDir: 'src',
-
-    middleware: [
-      historyApiFallback(),
-
-      webpackDevMiddleware(bundler, {
-        // Dev middleware can"t access config, so we provide publicPath
-        publicPath: config.output.publicPath,
-
-        // These settings suppress noisy webpack output so only errors are displayed to the console.
-        noInfo: true,
-        quiet: false,
-        stats: {
-          assets: false,
-          colors: true,
-          version: false,
-          hash: false,
-          timings: false,
-          chunks: false,
-          chunkModules: false,
-        },
-
-        // for other settings see
-        // https://webpack.js.org/guides/development/#using-webpack-dev-middleware
-      }),
-
-      // bundler should be the same as above
-      webpackHotMiddleware(bundler),
-    ],
+  proxy: {
+    target: 'localhost:8080',
+    ws: true,
   },
+  middleware: [
+    historyApiFallback(),
+
+    webpackDevMiddleware(bundler, {
+      // Dev middleware can"t access config, so we provide publicPath
+      publicPath: config.output.publicPath,
+
+      // These settings suppress noisy webpack output so only errors are displayed to the console.
+      noInfo: true,
+      quiet: false,
+      stats: {
+        assets: false,
+        colors: true,
+        version: false,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false,
+      },
+
+      // for other settings see
+      // https://webpack.js.org/guides/development/#using-webpack-dev-middleware
+    }),
+
+    // bundler should be the same as above
+    webpackHotMiddleware(bundler),
+  ],
 
   // no need to watch "*.js" here, webpack will take care of it for us,
   // including full page reloads if HMR won"t work
